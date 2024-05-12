cdec6e40de82badd65b93084ef991bc08dacc071
Shahriar Kariman <shahriar.kariman@gmail.com>
2024-05-11 23:25:59 -0300

    redeploying to debug.

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import glsl from 'vite-plugin-glsl';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  plugins: [glsl()]
+  plugins: [glsl()],
+  base: "https://Shahriar-Kariman.github.io/shader_practice_1/"
 });
